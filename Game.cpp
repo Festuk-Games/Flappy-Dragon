c1aed39f1b0e0e6a4e48c1d781a69f2ec44c133c
guillemal <guillem.alqueza@estudiantat.upc.edu>
2023-03-13 00:09:23 +0100

    Festuk Games

--- Game.cpp
+++ Game.cpp
@@ -32,7 +32,7 @@ bool Game::Init()
 		return false;
 	}
 	//Create our window: title, x, y, w, h, flags
-	Window = SDL_CreateWindow("Flappy Dragon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	Window = SDL_CreateWindow("Flappy Dragon - Festuk Games", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 	if (Window == NULL)
 	{
 		SDL_Log("Unable to create window: %s", SDL_GetError());
